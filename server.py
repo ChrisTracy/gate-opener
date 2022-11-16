7bc4a654dea4c2111992b5418e4bad1f58479c32
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2022-11-15 19:46:27 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,41 @@
-from tokenize import Token
-from flask import Flask
+from glob import glob
+from secrets import token_urlsafe
+from flask import Flask, request
 from flask_httpauth import HTTPTokenAuth
 from waitress import serve
+from pyairtable import Table
 import RPi.GPIO as GPIO
 import os
-import re
+import threading
 import time
 
-pin = os.environ['PIN']
+pin = 16
+
+def getTokens():
+    os.environ['AT_API_KEY'] = #AirTable API Key
+    os.environ['Base_ID'] = #AirTable Base ID
+    os.environ['Table_Name'] = 'users' #Table Name
+
+    at_api_key = os.environ['AT_API_KEY']
+    AT_BaseID  = os.environ['Base_ID']
+    AT_TableName = os.environ['Table_Name']
+
+    global table
+    table = Table(api_key=at_api_key, base_id=AT_BaseID, table_name=AT_TableName)
+    ATcontents = table.all()
+
+    global tokens
+    tokens = {}
+
+    for ATconent in ATcontents:
+        if "enabled" in ATconent['fields']:
+            tokenVal = ATconent['fields']['token']
+            userVal = ATconent['fields']['user']
+            tokens.update({tokenVal: userVal})
+
+    threading.Timer(30, getTokens).start()
+
+getTokens()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin,GPIO.OUT)
@@ -15,21 +43,6 @@ GPIO.output(pin,GPIO.HIGH)
 
 app = Flask(__name__)
 auth = HTTPTokenAuth(scheme='Bearer')
-
-
-list = os.environ
-tokens = {}
-
-for user in list:
-    if "user" in user:
-        Unum = re.findall(r'\d+', user)
-        for token in list:
-            if "token" in token:
-                Tnum = re.findall(r'\d+', token)
-                if Unum == Tnum:
-                    tokenVal = os.environ[token]
-                    userVal = os.environ[user]
-                    tokens.update({tokenVal: userVal})
 
 @auth.verify_token
 def verify_token(token):
@@ -40,6 +53,16 @@ def verify_token(token):
 @auth.login_required
 def index():
     return "Hello, {}!".format(auth.current_user())
+
+@app.route('/register', methods=["POST"])
+def register():
+    device = request.args.get('device')
+    key = request.args.get('key')
+
+    RawData = {"user": device, "token":key}
+    table.create(RawData)
+
+    return("Device key has been added. An admin must approve the request!")
 
 @app.route('/gate/front/', methods=["POST"])
 @auth.login_required
