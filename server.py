b8318a1ad4cb2007bfe6a0ad08e1f1b7806c8d69
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-10-07 00:17:48 -0500

    Update server.py

--- server.py
+++ server.py
@@ -24,25 +24,29 @@ logging.basicConfig(
 
 #function to pull tokens
 def getTokens():
-    logging.info('Getting tokens from AirTable')
-    at_api_key = os.environ['AT_API_KEY']
-    AT_BaseID  = os.environ['BASE_ID']
-    AT_TableName = os.environ['TABLE_NAME']
-    Token_Interval = os.environ['TOKEN_INTERVAL']
-    Token_Interval = int(Token_Interval)
+    try:
+        logging.info('Getting tokens from AirTable')
+        at_api_key = os.environ['AT_API_KEY']
+        AT_BaseID  = os.environ['BASE_ID']
+        AT_TableName = os.environ['TABLE_NAME']
+        Token_Interval = os.environ['TOKEN_INTERVAL']
+        Token_Interval = int(Token_Interval)
+    
+        global table
+        table = Table(api_key=at_api_key, base_id=AT_BaseID, table_name=AT_TableName)
+        ATcontents = table.all()
+    
+        global tokens
+        tokens = {}
+    except:
+        logging.exception("Could not reach Airtable!")
 
-    global table
-    table = Table(api_key=at_api_key, base_id=AT_BaseID, table_name=AT_TableName)
-    ATcontents = table.all()
-
-    global tokens
-    tokens = {}
-
-    for ATconent in ATcontents:
-        if "enabled" in ATconent['fields']:
-            tokenVal = ATconent['fields']['token']
-            userVal = ATconent['fields']['user']
-            tokens.update({tokenVal: userVal})
+    if ATcontents is not None:
+        for ATconent in ATcontents:
+            if "enabled" in ATconent['fields']:
+                tokenVal = ATconent['fields']['token']
+                userVal = ATconent['fields']['user']
+                tokens.update({tokenVal: userVal})
 
     threading.Timer(Token_Interval, getTokens).start()
 
@@ -66,13 +70,13 @@ def verify_token(token):
         return tokens[token]
 
 #welcome route
-@app.route('/welcome')
+@app.route('/api/v1/hello')
 @auth.login_required
 def index():
     return "Hello, {}!".format(auth.current_user())
 
 #register route
-@app.route('/register', methods=["POST"])
+@app.route('/api/v1/register', methods=["POST"])
 
 #register function
 def register():
@@ -85,7 +89,7 @@ def register():
     return "Device key has been added. An admin must approve the request!"
 
 #open route
-@app.route('/gate/front/', methods=["POST"])
+@app.route('/api/v1/trigger', methods=["POST"])
 @auth.login_required
 
 #open function
@@ -95,6 +99,36 @@ def open():
     GPIO.output(pin,GPIO.LOW)
     logging.info('Gate opened by {}'.format(auth.current_user()))
     return "Gate opened by {}!".format(auth.current_user())
+
+#refresh token route
+@app.route('/api/v1/refreshtokens', methods=["POST"])
+@auth.login_required
+
+#refresh tokens function
+def refreshTokens():
+    try:
+        logging.info('Getting tokens from AirTable')
+        at_api_key = os.environ['AT_API_KEY']
+        AT_BaseID  = os.environ['BASE_ID']
+        AT_TableName = os.environ['TABLE_NAME']
+        Token_Interval = os.environ['TOKEN_INTERVAL']
+        Token_Interval = int(Token_Interval)
+    
+        global table
+        table = Table(api_key=at_api_key, base_id=AT_BaseID, table_name=AT_TableName)
+        ATcontents = table.all()
+    
+        global tokens
+        tokens = {}
+    except:
+        logging.exception("Could not reach Airtable!")
+
+    if ATcontents is not None:
+        for ATconent in ATcontents:
+            if "enabled" in ATconent['fields']:
+                tokenVal = ATconent['fields']['token']
+                userVal = ATconent['fields']['user']
+                tokens.update({tokenVal: userVal})
 
 #start server
 if __name__ == "__main__":
