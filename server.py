f26b4d5c91c5cce8e4bc9593682e6f3c90b85fd1
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-10-08 23:39:46 -0500

    Update server.py

--- server.py
+++ server.py
@@ -130,7 +130,7 @@ def register():
         token = jwt.encode({'device':device, 'rand':num}, jwt_secret_key, algorithm='HS256')
     
         # Store the token and user/device information in Airtable
-        RawData = {"user": device, "auth": f"'device':{device}, 'rand':{num}"}
+        RawData = {"user": device, "auth": f"\"device\":\"{device}\", \"rand\":{num}"}
         table.create(RawData)
         logging.info('Registering new device: %s', device)
     
