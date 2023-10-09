5bdee9e31c62ed06ea579953b1e0b740fb923718
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-10-09 12:06:42 -0500

    Update server.py

--- server.py
+++ server.py
@@ -114,6 +114,7 @@ def verify_token(token):
         device_str = payload.get('device')
         current_user_name = get_user_by_device(ATcontents, device_str)
         logging.info(f"{device_str}")
+        logging.info(f"{current_user_name}")
         is_rand_in_auth = any(rand_value_str in element for element in auths)
         if is_rand_in_auth:
             logging.info(f"Token found! Auth Successful.")
