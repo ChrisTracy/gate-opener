776cc49baefff0ae4c65b2bf8d7c3ea7d1a0fecf
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2023-11-04 15:14:46 -0500

    Update pushover.py

--- notify/pushover.py
+++ notify/pushover.py
@@ -2,5 +2,5 @@ import requests
 
 def send_message(text,user,token):
     """Send a message"""
-    payload = {"message": text, "user": user, "api": token }
+    payload = {"message": text, "user": user, "token": token }
     r = requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
