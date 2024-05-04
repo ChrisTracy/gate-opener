dd6aac74f01b3a5dff5bb93a108e9b1bd4f04ed2
Chris Tracy <chris@christracy.com>
2024-05-04 00:56:13 -0500

    fix

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import threading
 import datetime
 import time
 import logging
@@ -9,9 +8,6 @@ from waitress import serve
 import sqlite3
 import os
 import RPi.GPIO as GPIO
-from modules.send_html_email import send_dynamic_email
-import modules.helper as helper
-import notify.pushover as pushover
 import config
 
 # Setup console logging and file logging
@@ -30,7 +26,8 @@ logging.basicConfig(
 )
 
 # Create SQLite database if not exists
-db_file = 'user_tokens.db'
+db_file = '/db/user_tokens.db'
+os.makedirs(os.path.dirname(db_file), exist_ok=True)
 if not os.path.exists(db_file):
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
@@ -38,27 +35,6 @@ if not os.path.exists(db_file):
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, auth TEXT, enabled BOOLEAN DEFAULT 0)''')
     conn.commit()
     conn.close()
-
-# Function to get tokens from SQLite
-def get_tokens(thread=False):
-    try:
-        logging.info('Getting tokens from SQLite')
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-        c.execute('SELECT user, auth FROM user_tokens')
-        auths = c.fetchall()
-        conn.close()
-        
-        if thread == True:
-            threading.Timer(config.token_interval, get_tokens, args=(True,)).start()
-        else:
-            return ('Token refresh completed')
-
-    except Exception as e:
-        logging.exception("Could not fetch tokens from SQLite: %s", str(e))
-
-# Run get tokens on the specified interval
-threading.Thread(target=get_tokens, args=(True,)).start()
 
 # Setup GPIO
 logging.info('Setting up GPIO on pin %s', config.pin)
@@ -81,14 +57,12 @@ def verify_token(token):
         device_str = payload.get('device')
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
-        c.execute('SELECT auth FROM user_tokens WHERE user = ?', (device_str,))
+        c.execute('SELECT auth FROM user_tokens WHERE user = ? AND enabled = 1', (device_str,))
         user_auth = c.fetchone()
         conn.close()
         if user_auth and rand_value_str in user_auth[0]:
             global current_user_name
-            global isAdmin
             current_user_name = device_str  # Assuming the device name is the user name
-            isAdmin = False  # No admin control in this version
             logging.info("Token found! Auth Successful for %s", current_user_name)
             return True
     except jwt.ExpiredSignatureError:
@@ -115,40 +89,47 @@ def register():
             # Create a JWT token with user/device information
             expiration_time = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=config.jwt_expiration_days)
             random_16_char_string = helper.generate_random_string(16)
-            invite_string = helper.generate_random_string(30)
-            host = config.proxy_url
             token = jwt.encode({'device': device, 'rand': random_16_char_string}, config.jwt_secret_key, algorithm='HS256')
     
-            # Store the token and user/device information in SQLite
+            # Store the token and user/device information in SQLite with enabled=False
             conn = sqlite3.connect(db_file)
             c = conn.cursor()
-            c.execute('INSERT INTO user_tokens (user, auth) VALUES (?, ?)', (device, f'{{"device":"{device}", "rand":"{random_16_char_string}"}}'))
+            c.execute('INSERT INTO user_tokens (user, auth, enabled) VALUES (?, ?, 0)', (device, f'{{"device":"{device}", "rand":"{random_16_char_string}"}}'))
             conn.commit()
             conn.close()
             logging.info('Registering new device: %s', device)
     
-            #send email
-            if config.sender_email and config.receiver_email and config.smtp_password and config.proxy_url:
-                # Define a dictionary of variables and their values
-                variables = {
-                    'friendly_name': config.friendly_name,
-                    'device_name': device,
-                    'host': config.proxy_url,
-                    'invite_str': invite_string,
-                    'psk': config.approval_psk,
-                }
-                
-                subject = f"New Device Request for {config.friendly_name}"
-                
-                # Call the send_dynamic_email function
-                send_dynamic_email(config.sender_email, config.receiver_email, config.smtp_server, config.smtp_port, config.smtp_username, config.smtp_password, subject, config.html_file_path, variables)
-    
-            return jsonify({"message": f"Your device ({device}) has been added to {config.friendly_name}. An admin must approve the request.", "host": host, "token": token})
+            return jsonify({"message": f"Your device ({device}) has been added to {config.friendly_name}. It requires admin approval.", "token": token})
         else:
             return jsonify({"message": "Missing the device parameter"})
     else:
         logging.exception('PSK did not match on register route. Provided PSK: %s', psk)
         return jsonify({"message": "Invalid PSK. Access denied."}), 401
+
+# Admin route - Approve user registration
+@app.route('/api/v1/admin/approve', methods=["POST"])
+def approve_user():
+    invite = request.args.get('invite')
+    psk = request.args.get('psk')
+
+    if psk == config.approval_psk: 
+        if invite:
+            try:
+                conn = sqlite3.connect(db_file)
+                c = conn.cursor()
+                c.execute('UPDATE user_tokens SET enabled = 1 WHERE auth = ?', (invite,))
+                conn.commit()
+                conn.close()
+                return f"User was approved. Invite: {invite}"
+            except Exception as e: 
+                logging.error(f"Failed to approve user with invite: {invite}. Error: {e}")
+                return f"Failed to approve user with invite: {invite}"
+        else:
+            logging.info("Invite not found in the URL")
+            return 'Invite not found in the URL'
+    else:
+       logging.exception('PSK did not match on enable route. Provided PSK: %s', psk)
+       return jsonify({"message": "Invalid PSK. Access denied."}), 401
 
 # Trigger route
 @app.route('/api/v1/trigger', methods=["POST"])
@@ -159,18 +140,8 @@ def trigger():
     GPIO.output(config.pin, GPIO.LOW)
 
     logging.info("%s opened by %s", config.friendly_name, current_user_name)
-    if config.pushover_user and config.pushover_token:
-        logging.info("Sending pushover notification for trigger route")
-        triggerMessage = f"{config.friendly_name} opened by {current_user_name}"
-        pushover.send_message(text=triggerMessage,user=config.pushover_user,token=config.pushover_token)
 
     return f"{config.friendly_name} opened by {current_user_name}"
-
-# Refresh token route
-@app.route('/api/v1/refreshtokens', methods=["POST"])
-@auth.login_required
-def refresh_tokens():
-    return ("Token refresh completed")
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=5151)
